95eaf8dc6f15b99c78b577955e2f0c2ef3bd09b0
at15 <at15@outlook.com>
2016-04-21 17:15:09 +0800

    Added swagger tools to use swagger UI
    but it can not load underscore.min.js, in node modules, I see
    lodash.min.js. Maybe it's time to update swagger express mw to 0.6.0
    and use Sway instead of swagger tools.
    
    related issue #3

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Swagger API for bform
 - due to https://github.com/at15/bform-api/issues/1, change must be made to installed modules
 > edit `node_modules/swagger/config/index.js` change `editorDir: path.resolve(config.nodeModules, 'swagger-editor'),` to `editorDir: path.dirname(require.resolve('swagger-editor'))`
 - `npm run edit`
+
+## Generate json
+
+`node util/convert.js` will generate a swagger.json file in `util` folder. You can use it to generate
+module and classes in other frameworks
--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var app = require('express')();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+    appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+    if (err) {
+        throw err;
+    }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // add swagger-ui
+    app.use(SwaggerUi(swaggerExpress.runner.swagger));
+    
+    // install middleware
+    swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+    var port = process.env.PORT || 10010;
+    app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+    if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/v1/hello?name=Scott');
+    }
 });
